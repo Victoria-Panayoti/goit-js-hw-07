3c1570352988ac26908b160d51c3276db5427e00
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-02-18 19:50:37 +0200

    made task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery');
+const imagesElements = galleryItems.map((item) => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    `
+}).join('');
+
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesElements);
+imagesContainer.addEventListener('click', onImagesElementClick);
+
+ function onImagesElementClick(event) {
+    event.preventDefault()
+    
+    const isGalleryImageEl = event.target.nodeName
+    if (isGalleryImageEl !== 'IMG') {
+        return;
+    };
+ 
+    const lightbox = new SimpleLightbox(`.gallery a`, {
+        loop: true,
+        captionsData: `alt`,
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        showCounter: false,
+    });
+}
